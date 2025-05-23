d1b604d59ac1a31b6caa3d8d63124daf020cbf06
scott christian mccallum <scottcmccallum@gmail.com>
2025-05-23 16:40:34 -0400

    config

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,15 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
-	}
+const dev = process.env.NODE_ENV === 'development';
+
+export default {
+  kit: {
+    adapter: adapter(),
+    paths: {
+      base: dev ? '' : '/kmclt' // REPLACE with your GitHub repo name
+    },
+    prerender: {
+      default: true
+    }
+  }
 };
-
-export default config;
