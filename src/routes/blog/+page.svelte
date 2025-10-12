c7aa84b1b07cf20eefe9d260276436904bbaf52c
scott christian mccallum <scottcmccallum@gmail.com>
2025-10-12 14:45:47 -0400

    add data

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -26,7 +26,10 @@
             <img src={kmcltImage} alt="Kensington Market" class="logo" />
         </a>
         <h1>Mapping change in Kensington Market</h1>
-        <p><a href="">Farzaneh Hemmasi</a>, <a href="">Emily Hertzman</a>, <a href="" >Scott McCallum</a> | Month 2025</p>
+        <p>
+            <a href="">Farzaneh Hemmasi</a>, <a href="">Emily Hertzman</a>,
+            <a href="">Scott McCallum</a> | Month 2025
+        </p>
 
         <div class="blog-text">
             <p>
@@ -55,6 +58,39 @@
         </div>
 
         <div class="sankey"></div>
+
+        <div class="blog-text">
+            <h2>The Disappearance of Small Grocers</h2>
+            <p>
+                We were interested in mapping two major trends observed in
+                Kensington Market over the past 20 years: (1) the disappearance
+                of small grocers and (2) the rise of cannabis stores.
+            </p>
+            <p>
+                We were particularly interested in mapping the change of these
+                two categories as they symbolize the broader shift in Kensington
+                Market from a historically livable neighbourhood, where
+                amenities for everyday life such as produce and raw food stores
+                are accessible and close by from a variety of small businesses,
+                to a retail destination, largely centered around shopping and
+                leisure.
+            </p>
+
+            <p>
+                The proliferation of cannabis stores is both notable and
+                noticeable. In 2025 there were 5 cannabis stores in the
+                neighbourhood. For a neighbourhood the size of Kensington Market
+                to have this many cannabis stores is telling of the broader
+                change from serving the local community to serving customers
+                from outside the neighbourhood.
+            </p>
+            <p>
+                Increasingly a site of leisure and consumption, landlords may
+                have favoured higher-paying tenants, such as cannabis retailers,
+                over other businesses such as traditional grocers. Rents go up,
+                local amenities get priced out, the neighbourhood changes.
+            </p>
+        </div>
     </div>
 
     <!-- Timeline and Map with shared legend header -->
@@ -72,7 +108,11 @@
                 />
             </div>
             <div class="map-section">
-                <MapTimeline bind:sliderYear bind:hoveredAddress {enabledTypes} />
+                <MapTimeline
+                    bind:sliderYear
+                    bind:hoveredAddress
+                    {enabledTypes}
+                />
             </div>
         </div>
     </div>
